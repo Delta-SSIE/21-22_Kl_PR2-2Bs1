09225074ee8597e9272b180b572221db7c738bcd
Jan Koupil <jan.koupil@delta-skola.cz>
2021-09-06 09:30:42 +0200

    První lekce - datové typy a řetězce

--- 01-Hello-world/Program.cs
+++ 01-Hello-world/Program.cs
@@ -6,7 +6,35 @@ namespace _01_Hello_world
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+
+            int cislo = 13;
+
+            string stav = cislo > 10 ? "velké" : "malé";
+
+            //if (cislo > 10)
+            //{
+            //    stav = "velke";
+            //}
+            //else
+            //{
+            //    stav = "malé";
+            //}
+
+            string stav2 =    cislo < 3 ? "prťavé" 
+                            : cislo < 5 ? "malé"
+                            : cislo  < 10 ? "tak akorát"
+                            : "velké";
+
+            Console.WriteLine("Číslo je " + cislo + ".");
+            Console.WriteLine("Číslo je {0}.", cislo);
+            Console.WriteLine($"Číslo je {cislo}.");
+
+
+
+
+
+
         }
     }
 }
