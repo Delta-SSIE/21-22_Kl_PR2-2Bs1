c599d31d996fe8982a103b789a2db890c62fd6b7
Jan Koupil <jan.koupil@delta-skola.cz>
2021-09-13 09:14:37 +0200

    2. opakovací lekce a prohození proměnných

--- 01-Hello-world/Program.cs
+++ 01-Hello-world/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace _01_Hello_world
+namespace _01_Uvod_00_Opakovani_syntaxe
 {
     class Program
     {
@@ -8,9 +8,9 @@ namespace _01_Hello_world
         {
             //Console.WriteLine("Hello World!");
 
-            int cislo = 13;
+            //int cislo = 13;
 
-            string stav = cislo > 10 ? "velké" : "malé";
+            //string stav = cislo > 10 ? "velké" : "malé";
 
             //if (cislo > 10)
             //{
@@ -21,20 +21,58 @@ namespace _01_Hello_world
             //    stav = "malé";
             //}
 
-            string stav2 =    cislo < 3 ? "prťavé" 
-                            : cislo < 5 ? "malé"
-                            : cislo  < 10 ? "tak akorát"
-                            : "velké";
+            //string stav2 =    cislo < 3 ? "prťavé" 
+            //                : cislo < 5 ? "malé"
+            //                : cislo  < 10 ? "tak akorát"
+            //                : "velké";
 
-            Console.WriteLine("Číslo je " + cislo + ".");
-            Console.WriteLine("Číslo je {0}.", cislo);
-            Console.WriteLine($"Číslo je {cislo}.");
+            //Console.WriteLine("Číslo je " + cislo + ".");
+            //Console.WriteLine("Číslo je {0}.", cislo);
+            //Console.WriteLine($"Číslo je {cislo}.");
+
+            //int i = 20;
+            //do
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //} while (i < 20);
+
+            //int cislo;
+            //bool poprve = true;
+            //do
+            //{
+            //    if (poprve)
+            //    {
+            //        poprve = false;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Chyba, zkus to znovu");
+            //    }
+
+            //    Console.WriteLine("Zadej číslo mezi 1 a 10");
+
+            //    string odpoved = Console.ReadLine();
+            //    cislo = int.Parse(odpoved);
+            //}
+            //while (cislo > 10 || cislo < 1);
+            //Console.WriteLine($"Zadal jsi {cislo}.");
 
 
+            //for (int cislo = 1; cislo < 20; cislo++)
+            //{
+            //    if (cislo % 8 == 0)
+            //        continue; 
 
+            //    Console.WriteLine(cislo);
+            //}
+            int a = 10;
+            int b = 15;
+            a = a ^ b;
+            b = a ^ b;
+            a = a ^ b;
 
-
-
+            Console.WriteLine($"a: {a}, b: {b}");
         }
     }
 }
---
+++ 01-Uvod_02-Prohozeni-promennych/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _01_Uvod_01_Prohozeni_promennych
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 20;
+
+            // pomocná proměnná
+            //int tmp = x;
+            //x = y;
+            //y = tmp;
+
+            // sčítání
+            //x = x + y;
+            //y = x - y;
+            //x = x - y;
+
+            // xor
+
+            x = x ^ y;
+            y = x ^ y;
+            x = x ^ y;
+
+            Console.WriteLine($"x: {x}, y: {y}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
