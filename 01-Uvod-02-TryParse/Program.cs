a5a31bcabeaebfbab8c8233abf7b51e5fd8c07ef
Jan Koupil <jan.koupil@gmail.com>
2021-09-20 09:30:12 +0200

    2. opakovací lekce - TryParse

---
+++ 01-Uvod-02-TryParse/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _01_Uvod_02_TryParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Zadej celé číslo: ");
+            //string input = Console.ReadLine();
+
+            //int cislo;
+            //if (int.TryParse(input, out cislo))
+            //{
+            //    Console.WriteLine($"Číslo je {cislo}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("To nejde převést");
+            //}
+
+
+            Console.WriteLine("Zadej celé číslo: ");
+            string input = Console.ReadLine();
+            int cislo;
+            while (!int.TryParse(input, out cislo))
+            {
+                Console.WriteLine("Zadej celé číslo, truhlíku! Zkus to znova");
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
