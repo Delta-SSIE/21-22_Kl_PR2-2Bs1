e90a9220a7dda3c867b9f41de51e078b55e8d5a2
Jan Koupil <jan.koupil@gmail.com>
2021-10-04 12:34:52 +0200

    Demo - Enumerace

---
+++ 01-OOP-Enum-Demo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _01_OOP_Enum_Demo
+{
+    enum Obdobi:byte { Jaro = 1, Leto, Podzim = 5, Zima }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //Obdobi chladne = Obdobi.Jaro;
+            //Console.WriteLine(chladne);
+
+            //chladne++;
+            //Console.WriteLine(chladne);
+
+            //chladne++;
+            //chladne++;
+            //Console.WriteLine(chladne);
+            //chladne++;
+            //Console.WriteLine(chladne);
+
+            //Obdobi horke = (Obdobi)1;
+            //Console.WriteLine(horke);
+
+            Obdobi barevne = Obdobi.Podzim;
+            Console.WriteLine((int)barevne);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ 01-Uvod-Obraceni/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _01_Uvod_Obraceni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string vstup = "soubory na disku nenalezeny!";
+            //string obracene = new String(vstup.Reverse().ToArray());
+
+            string obr = "";
+            for (int i = vstup.Length - 1; i >= 0; i++)
+            {
+                obr += vstup[i];
+            }
+            "kobyla má malý bok"
+        }
+    }
+}
