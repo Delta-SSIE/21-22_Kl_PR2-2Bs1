e1669044913287b82fd46c064336465842105824
Jan Koupil <jan.koupil@gmail.com>
2021-10-25 09:36:12 +0200

    25.10.2022 Balicek

---
+++ 02-OOP-04-Fronta/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02_OOP_04_Fronta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //nová fronta na čísla
+            Queue<int> numbers = new Queue<int>();
+
+            //délka
+            Console.WriteLine($"Ve frontě je {numbers.Count} položek : {string.Join(", ", numbers)}.");
+
+            numbers.Enqueue(13);
+
+            Console.WriteLine($"Ve frontě je {numbers.Count} položek : {string.Join(", ", numbers)}.");
+            numbers.Enqueue(17);
+
+            Console.WriteLine($"Ve frontě je {numbers.Count} položek : {string.Join(", ", numbers)}.");
+            int next;
+            
+            next = numbers.Dequeue();
+            Console.WriteLine($"Na řadě je {next}.");
+            Console.WriteLine($"Ve frontě je {numbers.Count} položek : {string.Join(", ", numbers)}.");
+
+            numbers.Enqueue(19);
+            numbers.Enqueue(23);
+            numbers.Enqueue(29);
+            Console.WriteLine($"Ve frontě je {numbers.Count} položek : {string.Join(", ", numbers)}.");
+
+            next = numbers.Dequeue();
+            Console.WriteLine($"Na řadě je {next}.");
+            Console.WriteLine($"Ve frontě je {numbers.Count} položek : {string.Join(", ", numbers)}.");
+        }
+    }
+}
---
+++ 02-OOP-Karty/Balicek.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_OOP_Karty
+{
+    class Balicek
+    {
+        //úložiště karet
+        private Queue<Karta> _karty = new Queue<Karta>();
+
+        //generátor náh. čísel
+        private Random _rnd = new Random();
+
+        public int PocetKaret 
+        { 
+            get
+            {
+                return _karty.Count;
+            }
+        }
+
+        //Zamichej
+        public void Zamichej()
+        {
+            // vezmu všechny karty v balíčku
+            Karta[] tmp = _karty.ToArray();
+
+            for (int i = 0; i < tmp.Length - 1; i++)
+            {
+                int nahPoradi = _rnd.Next(i, tmp.Length);
+                
+                Karta vyndej = tmp[nahPoradi];
+                tmp[nahPoradi] = tmp[i];
+                tmp[i] = vyndej;
+            }
+
+            _karty = new Queue<Karta>(tmp);
+        }
+
+        public Karta VezmiHorni()
+        {
+            Karta horni = _karty.Dequeue();
+            return horni;
+        }
+
+        public void PridejDospod(Karta karta)
+        {
+            _karty.Enqueue(karta);
+        }
+
+        public void Vypis()
+        {
+            foreach (Karta karta in _karty)
+            {
+                Console.WriteLine(karta);
+            }
+        }
+    }
+}
