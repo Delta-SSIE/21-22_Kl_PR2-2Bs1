982a2ed331f5a78f84675a5f11e44ec2b90fc3c8
Jan Koupil <jan.koupil@delta-skola.cz>
2021-11-22 09:24:49 +0100

    2021-11-22 Vlastnosti

---
+++ 02-OOP-07-Kruznice-II/Kruznice.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_OOP_07_Kruznice_II
+{
+    class Kruznice
+    {
+
+        private double _polomer;
+        public double Polomer 
+        { 
+            set
+            {
+                if (value >= 0) //když je OK
+                    _polomer = value;
+                else
+                    throw new ArgumentOutOfRangeException("Negative diameter");
+            }
+            get
+            {
+                return _polomer;
+            }
+        }
+
+        public Kruznice()
+        {
+            Polomer = 0;
+        }
+
+        public Kruznice(double polomer)
+        {
+            Polomer = polomer;
+        }
+
+
+        public double Plocha
+        {
+            get
+            {
+                return Math.PI * _polomer * _polomer;
+            }            
+        }
+    }
+}
---
+++ 02-OOP-07-Kruznice-II/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _02_OOP_07_Kruznice_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Kruznice k = new Kruznice();
+                k.Polomer = -1;
+                Console.WriteLine($"Obsah kružnice o poloměru {k.Polomer} je {k.Plocha}");
+                //k.Plocha = 6;
+            }
+            catch
+            {
+                Console.WriteLine("Neplatná hodnota poloměru");
+            }
+
+            try
+            {
+                Kruznice k = new Kruznice(6);
+                Console.WriteLine($"Obsah kružnice o poloměru {k.Polomer} je {k.Plocha}");
+            }
+            catch
+            {
+                Console.WriteLine("Neplatná hodnota poloměru");
+            }
+        }
+    }
+}
