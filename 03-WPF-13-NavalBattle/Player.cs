5821a7d2843bcb48e45c82b9b0be931091025455
Jan Koupil <jan.koupil@gmail.com>
2022-02-07 10:30:06 +0100

    2022-02-07 - Zaklad - namorni bitva

---
+++ 03-WPF-13-NavalBattle/Player.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_WPF_13_NavalBattle
+{
+    class Player
+    {
+        private int _mapSize;
+        private int _shipCount;
+
+        /// <summary>
+        /// Holds the real state of player sea
+        /// </summary>
+        private TileState[,] _sea;
+
+        public TileState[,] RealSea
+        {
+            get
+            {
+                return (TileState[,])(_sea.Clone());
+            }
+        }
+        /// <summary>
+        /// Provides sea with masked ship positions for render
+        /// </summary>
+        public TileState[,] VisibleSea
+        {
+            get
+            {
+                TileState[,] sea = (TileState[,])(_sea.Clone());
+                
+                for (int x = 0; x < _mapSize; x++)
+                {
+                    for (int y = 0; y < _mapSize; y++)
+                    {
+                        if (sea[x, y] == TileState.Ship)
+                            sea[x, y] = TileState.Sea;
+                    }
+                }
+
+                return sea;
+            }
+        }
+        public int Wrecks { get; private set; }
+        public bool IsAlive
+        {
+            get
+            {
+                return Wrecks < _shipCount;
+            }
+        }
+        private void CreateSea()
+        {
+            _sea = new TileState[_mapSize, _mapSize]; //automatically sets all values to TileState.Sea (=0)
+        }
+        private void PlaceShips()
+        {
+            Random rnd = new Random();
+            
+            int placedShips = 0;
+            
+            while (placedShips < _shipCount)
+            {
+                int x = rnd.Next(_mapSize);
+                int y = rnd.Next(_mapSize);
+                if (_sea[x,y] != TileState.Ship)
+                {
+                    _sea[x, y] = TileState.Ship;
+                    placedShips++;
+                }
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <returns>True on hitting a new ship, false otherwise</returns>
+        public bool HandleShot(Coordinates target)
+        {
+            switch (_sea[target.X, target.Y])
+            {
+                case TileState.Sea:
+                    _sea[target.X, target.Y] = TileState.Shot;
+                    return false;
+
+                case TileState.Ship:
+                    _sea[target.X, target.Y] = TileState.Wreck;
+                    Wrecks++;
+                    return true;
+
+                default:
+                    return false;
+            }
+        }
+    }
+}
---
+++ 03-WPF-13-NavalBattle/TileState.cs
@@ -0,0 +1,4 @@
+﻿namespace _03_WPF_13_NavalBattle
+{
+    public enum TileState : byte { Sea, Ship, Shot, Wreck }
+}
