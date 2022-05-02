3174cf97fca5bf757bf866508372d5d16171267d
Jan Koupil <jan.koupil@delta-skola.cz>
2022-05-02 09:29:00 +0200

    Dedicnost, Iface - reseni

---
+++ 04-OOP-Dedicnost-06-Iface-IComparable/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace _04_OOP_Dedicnost_06_Iface_IComparable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Strom[] sad = new Strom[]
+            {
+                new Strom() {Druh = "Jabloň", Vyska = 5.5},
+                new Strom() {Druh = "Smrk", Vyska = 12},
+                new Strom() {Druh = "Hrušeň", Vyska = 8.2},
+                new Strom() {Druh = "Třešeň", Vyska = 3.3},
+                new Strom() {Druh = "Švestka", Vyska = 4.1}
+            };
+
+            foreach (Strom strom  in sad)
+            {
+                Console.WriteLine($"{strom.Druh}: {strom.Vyska}");                
+            }
+
+            Array.Sort(sad);
+
+            foreach (Strom strom in sad)
+            {
+                Console.WriteLine($"{strom.Druh}: {strom.Vyska}");
+            }
+
+            
+        }
+    }
+
+    class Strom : IComparable
+    {
+        public string Druh { get; set; }
+        public double Vyska { get; set; }
+
+        //public int CompareTo(object obj)
+        //{
+        //    return Vyska.CompareTo(obj);
+        //}
+
+
+        //int IComparable.CompareTo(object obj)
+        //{
+        //        //převést na strom
+        //        Strom druhyStrom = (Strom)obj;
+
+        //        if (this.Vyska < druhyStrom.Vyska)
+        //            return -1;
+        //        else if (this.Vyska == druhyStrom.Vyska)
+        //            return 0;
+        //        else
+        //            return 1;
+        //}
+
+
+        public int CompareTo(object obj)
+        {
+            //převést na strom
+            Strom druhyStrom = (Strom)obj;
+
+            if (this.Vyska < druhyStrom.Vyska)
+                return -1;
+            else if (this.Vyska == druhyStrom.Vyska)
+                return 0;
+            else
+                return 1;
+            //return (int)Math.Ceiling(this.Vyska - druhyStrom.Vyska);
+        }
+    }
+}
---
+++ replit-03-OOP2-060-Iface-Utvary/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace replit_03_OOP2_060_Iface_Utvary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Obdelnik o = new Obdelnik(10, 5);
+            Ctverec c = new Ctverec(5);
+            Trojuhelnik t = new Trojuhelnik(3, 4, 5);
+            Kruh k = new Kruh(2);
+
+            IUtvar[] utvary = new IUtvar[4];
+            utvary[0] = o;
+            utvary[1] = c;
+            utvary[2] = t;
+            utvary[3] = k;
+
+            double sumObvod = 0;
+            double sumObsah = 0;
+            foreach(IUtvar u in utvary)
+            {
+                Console.WriteLine(u.ToString());
+                sumObvod += u.GetObvod();
+                sumObsah += u.GetObsah();
+            }
+            Console.WriteLine("Obvod celkem: " + sumObvod);
+            Console.WriteLine("Obsah celkem: " + sumObsah);
+
+
+        }
+
+        static void Popis(IUtvar utvar)
+        {
+            Console.WriteLine($"{utvar.Nazev} má obvod {utvar.GetObvod()} a obsah {utvar.GetObsah()}");
+
+        }
+    }
+}
